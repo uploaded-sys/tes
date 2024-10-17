034fda689ef0631e1fde6760e42d14ae538a0d1d
Uploaded systems <sahoorajashree01@gmail.com>
2024-10-17 15:53:57 +0530

    added new interactivity
    changed the the website to a new one added css html and javascript

---
+++ lolo.js
@@ -0,0 +1,37 @@
+function ji() {
+    console.log('status: working........')
+    let v = document.getElementById('ma')
+    v.innerText = "yo you, im your new friend"
+}
+function reset() {
+    console.log('status: working........')
+    let v = document.getElementById('ma')
+    v.innerText = 'test websiite'
+}
+
+function bi() {
+    console.log('status: working........')
+    let v = document.getElementById('ma')
+    v.innerText = "ahhaa you bad organisim"
+    v.innerText = "you ....... yaaa .... dumb...."
+}
+let count = 0
+function de() {
+    console.log('status: working........')
+    let v = document.getElementById('ba')
+    count = count+1
+    v.innerText = count+1
+}
+
+function ia() {
+    console.log('status: working........')
+    let v = document.getElementById('ba')
+    v.innerText = count-1
+    count = count-1
+}
+function reset2() {
+    console.log('status: working........')
+    let v = document.getElementById('ba')
+    count = 0
+    v.innerText = count
+}
