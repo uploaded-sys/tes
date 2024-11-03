46a6df85f875d6280b43c518d890e0e557906825
Uploaded systems <sahoorajashree01@gmail.com>
2024-11-03 01:29:00 -0700

    updated a bit

---
+++ quad.js
@@ -0,0 +1,14 @@
+function a1() {
+  let p2_ed = document.getElementById("ed");
+  p2_ed.innerText = "AHH YOU ARE MAKING ME ANGRY.....";
+}
+function a2() {
+  console.log("a2 exicuted");
+  let p2_ed = document.getElementById("ed");
+  p2_ed.innerText = "OO YOURE SO NICE......";
+}
+function a3() {
+  console.log("a3 exicuted");
+  let p2_ed = document.getElementById("ed");
+  p2_ed.innerText = "HI IM JIGINESH";
+}
