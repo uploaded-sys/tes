705389f5a181d14eda444eeac2eb1158cc80d9e3
Uploaded systems <sahoorajashree01@gmail.com>
2024-11-03 01:44:44 -0800

    added new things
    completed the project and added credits and nice animations

--- quad.js
+++ quad.js
@@ -1,5 +1,6 @@
 function a1() {
   let p2_ed = document.getElementById("ed");
+  console.log("a1 exicuted");
   p2_ed.innerText = "AHH YOU ARE MAKING ME ANGRY.....";
 }
 function a2() {
@@ -11,4 +12,31 @@ function a3() {
   console.log("a3 exicuted");
   let p2_ed = document.getElementById("ed");
   p2_ed.innerText = "HI IM JIGINESH";
+}
+let counter = Number(localStorage.getItem("lastval"))
+let lsd = localStorage.setItem('lastval',counter)
+function fetch_last_data(){
+  let p4_hp = document.getElementById('ped')
+  p4_hp.innerText = 'counter: ' + counter;
+}
+function b1() {
+  let p4_hp = document.getElementById('ped')
+  console.log("b1 exicuted");
+  counter += 1
+  localStorage.setItem('lastval', String(counter))
+  p4_hp.innerText = 'counter: ' + counter;
+}
+function b2() {
+  let p4_hp = document.getElementById('ped')
+  console.log("b2 exicuted");
+  counter -= 1
+  localStorage.setItem('lastval', String(counter))
+  p4_hp.innerText = 'counter: ' + counter;
+}
+function b3() {
+  let p4_hp = document.getElementById('ped')
+  console.log("b3 exicuted");
+  counter = 0
+  localStorage.setItem('lastval', String(counter))
+  p4_hp.innerText = 'counter: ' + counter;
 }
